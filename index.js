de6a2fc4e072ce93d77d18b0bb45a555c2ea36bf
thmmahendra <thmmahendra@gmail.com>
2024-02-05 21:25:23 +0545

    Initial Commit

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ canvas.height = 576
 
 const gravity = 0.5
 
+class Sprite {
+    constructor({ position, imageSrc }) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    update() {
+        this.draw()
+    }
+}
+
 class Player {
     constructor(position) {
         this.position = position
@@ -51,10 +68,20 @@ const keys = {
     },
 }
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    background.update()
     player.update()
     player2.update()
 
